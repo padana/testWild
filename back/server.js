e95b2e2c4ee7a90c8fc8ce4535fd69666238ad37
padana <56067585+padana@users.noreply.github.com>
2021-02-22 15:56:51 +0100

    Update server.js

--- back/server.js
+++ back/server.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
 const json = require('express-json');
 const cors = require('cors')
 const app = express()
 const port = 3030
 
 //DB
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/argo', {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
+
+const DB = require('./controllers/key').MongoURI
+mongoose.connect(DB, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
